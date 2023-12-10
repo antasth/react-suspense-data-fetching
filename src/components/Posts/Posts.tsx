5edf58aa73e3ab8efff031978f1cdfea36f73028
antasth <antasth@mail.ru>
2023-12-10 21:32:41 +0300

    feat: add react-suspend

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,3 +1,4 @@
+import { suspend } from 'suspend-react'
 import styles from './Post.module.css'
 
 interface IPost {
@@ -6,42 +7,33 @@ interface IPost {
   title: string
   body: string
 }
-// const endpoint = 'https://jsonplaceholder.typicode.com/posts'
-//     const getData = async () => {
-//       try {
-//         const response = await fetch(endpoint)
-//         const data = await response.json()
-//         const posts = data
-//         return posts
-//       } catch (error) {
-//         console.log(error)
-//       }
-//     }
-//     getData()
 
-let posts: IPost[]
-const promise = new Promise((resolve) => {
-  setTimeout(() => {
-    posts = [{ userId: 6, id: '5', title: 'Title', body: 'body' }]
-    resolve()
-  }, 2000)
-})
+const endpoint = 'https://jsonplaceholder.typicode.com/posts?_limit=5'
+
+const getData = async (): Promise<IPost[] | undefined> => {
+  try {
+    const response = await fetch(endpoint)
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const Posts = (): React.ReactElement => {
   console.log('render')
 
-  if (!posts) {
-    console.log('posts', posts)
-    throw promise
-  }
+  const posts = suspend(getData, [])
+
   return (
     <div className={styles.posts}>
-      {posts.map((post) => (
-        <div key={post.id} className={styles.post}>
-          <h3>id: {post.id}</h3>
-          <h3>title: {post.title}</h3>
-          <p>{post.body}</p>
-        </div>
-      ))}
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id} className={styles.post}>
+            <h3>id: {post.id}</h3>
+            <h3>title: {post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
     </div>
   )
 }
