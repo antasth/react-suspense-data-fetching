b9912c5e0871a8b0da90d0bccaab2009101cb019
antasth <antasth@mail.ru>
2023-12-10 20:21:13 +0300

    feat: add Suspense and promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from 'react'
 import './App.css'
 import { Posts } from './components/Posts/Posts'
 
 function App() {
-  return <Posts />
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Posts />
+    </Suspense>
+  )
 }
 
 export default App
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import styles from './Post.module.css'
 
 interface IPost {
@@ -7,22 +6,33 @@ interface IPost {
   title: string
   body: string
 }
-export const Posts = (): React.ReactElement => {
-  const [posts, setPosts] = useState<IPost[]>([])
-  const endpoint = 'https://jsonplaceholder.typicode.com/posts'
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(endpoint)
-        const data = await response.json()
-        setPosts(data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    getData()
-  }, [])
+// const endpoint = 'https://jsonplaceholder.typicode.com/posts'
+//     const getData = async () => {
+//       try {
+//         const response = await fetch(endpoint)
+//         const data = await response.json()
+//         const posts = data
+//         return posts
+//       } catch (error) {
+//         console.log(error)
+//       }
+//     }
+//     getData()
 
+let posts: IPost[]
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    posts = [{ userId: 6, id: '5', title: 'Title', body: 'body' }]
+    resolve()
+  }, 2000)
+})
+export const Posts = (): React.ReactElement => {
+  console.log('render')
+
+  if (!posts) {
+    console.log('posts', posts)
+    throw promise
+  }
   return (
     <div className={styles.posts}>
       {posts.map((post) => (
